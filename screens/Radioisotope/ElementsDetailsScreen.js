e20765beaa0ae76a5a2ae04df9106373c2d4ebeb
Prathima0708 <kotianpriya64@gmail.com>
2024-01-20 17:15:03 +0530

    updated corrections

--- screens/Radioisotope/ElementsDetailsScreen.js
+++ screens/Radioisotope/ElementsDetailsScreen.js
@@ -1,93 +1,105 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, ScrollView, Dimensions } from 'react-native'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    ScrollView,
+    Dimensions,
+} from 'react-native'
 import Header from '../../components/Header'
 import { COLORS } from '../../constants'
+import { SafeAreaView } from 'react-native'
 
 const ElementsDetailsScreen = ({ route }) => {
     const { elementDetails } = route.params
 
     return (
-        <ScrollView style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Header title={`${elementDetails.name} details`} />
+            <ScrollView>
+                <View style={styles.detailsContainer}>
+                    <Text style={styles.detailText}>
+                        Symbol: {elementDetails.symbol}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Period: {elementDetails.period}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Group: {elementDetails.group}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Atomic Number: {elementDetails.atomicNumber}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Atomic Weight: {elementDetails.atomicWeight}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Energy Type: {elementDetails.energyType}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Energy : {elementDetails.energy}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Half Life: {elementDetails.halfLife}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Production Method: {elementDetails.productionMethod}
+                    </Text>
 
-            <View style={styles.detailsContainer}>
-                <Text style={styles.detailText}>
-                    Symbol: {elementDetails.symbol}
-                </Text>
-                <Text style={styles.detailText}>
-                    Period: {elementDetails.period}
-                </Text>
-                <Text style={styles.detailText}>
-                    Group: {elementDetails.group}
-                </Text>
-                <Text style={styles.detailText}>
-                    Atomic Number: {elementDetails.atomicNumber}
-                </Text>
-                <Text style={styles.detailText}>
-                    Atomic Weight: {elementDetails.atomicWeight}
-                </Text>
-                <Text style={styles.detailText}>
-                    Energy Type: {elementDetails.energyType}
-                </Text>
-                <Text style={styles.detailText}>
-                    Energy : {elementDetails.energy}
-                </Text>
-                <Text style={styles.detailText}>
-                    Half Life: {elementDetails.halfLife}
-                </Text>
-                <Text style={styles.detailText}>
-                    Production Method: {elementDetails.productionMethod}
-                </Text>
-
-                {/* <Text style={styles.detailText}>
+                    {/* <Text style={styles.detailText}>
                     Type of energy emission: {elementDetails.energyType}
                 </Text> */}
-                <Text style={styles.detailText}>
-                    Oxidation state: {elementDetails.oxidationstate}
-                </Text>
+                    <Text style={styles.detailText}>
+                        Oxidation state: {elementDetails.oxidationstate}
+                    </Text>
 
-                <Text style={styles.detailText}>
-                    Parent Radionuclide: {elementDetails.parentRadionuclide}
-                </Text>
-                <Text style={styles.detailText}>
-                    Decay Product : {elementDetails.decayproduct}
-                </Text>
-                <Text style={styles.detailText}>
-                    Natural Structure : {elementDetails.naturalstructure}
-                </Text>
-                {elementDetails.decayimage && (
-                    <React.Fragment>
-                        <Text style={styles.detailText}> Decay Scheme:</Text>
-                        <Image
-                            source={elementDetails.decayimage}
-                            style={{
-                                height: 200,
-                                width: 270,
-                                resizeMode: 'contain',
-                            }}
-                        />
-                    </React.Fragment>
-                )}
+                    <Text style={styles.detailText}>
+                        Parent Radionuclide: {elementDetails.parentRadionuclide}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Decay Product : {elementDetails.decayproduct}
+                    </Text>
+                    <Text style={styles.detailText}>
+                        Natural Structure : {elementDetails.naturalstructure}
+                    </Text>
+                    {elementDetails.decayimage && (
+                        <React.Fragment>
+                            <Text style={styles.detailText}>
+                                {' '}
+                                Decay Scheme:
+                            </Text>
+                            <Image
+                                source={elementDetails.decayimage}
+                                style={{
+                                    height: 200,
+                                    width: 270,
+                                    resizeMode: 'contain',
+                                }}
+                            />
+                        </React.Fragment>
+                    )}
 
-                {/* <Image
+                    {/* <Image
                     source={elementDetails.decayimage}
                     //style={{ height: 100, width: 200 }}
                 /> */}
 
-                {/* Add more details as needed */}
-            </View>
-            <View style={{ height: 70 }} />
-        </ScrollView>
+                    {/* Add more details as needed */}
+                </View>
+                <View style={{ height: 50 }} />
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 export default ElementsDetailsScreen
-const {height,width}=Dimensions.get('window')
+const { height, width } = Dimensions.get('window')
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: COLORS.white,
-        paddingTop: width*0.13,
+        paddingTop: width * 0.13,
         // marginBottom:10
     },
     detailsContainer: {
